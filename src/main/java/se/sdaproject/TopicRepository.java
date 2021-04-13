3ebc4fd43f2090726baf82bbcab6737b89824b0e
olhapinchuk <pinchukolya@gmail.com>
2021-04-13 21:31:33 +0200

    add corrections to app structure

--- src/main/java/se/sdaproject/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package se.sdaproject;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicRepository extends JpaRepository<Topic, Long>{
-    boolean existsByName(String name);
-}
